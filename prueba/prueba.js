558b2aa35b6507c1e7e49446df206a34f9733602
Ariel-Navarro <arielknm@gmail.com>
2023-01-30 18:22:01 -0500

    diagrama de flujo

---
+++ prueba/prueba.js
@@ -0,0 +1,11 @@
+// const fs=require('fs');
+// fs.readFile('data.txt', 'utf-8', (error,data) => {
+//   if (error){
+//     console.log(`Error ${error}`)
+//   }else {
+//     console.log(data)
+//   }
+// })
+const path=require('path');
+pathabsolute=path.resolve('prueba')
+console.log(pathabsolute);
