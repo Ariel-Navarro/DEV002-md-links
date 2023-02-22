f06b89f924cdb0745ac5e2ec27e78494e3b84443
Ariel-Navarro <arielknm@gmail.com>
2023-02-22 03:49:07 -0500

    mdLinks

--- prueba/api2.js
+++ prueba/api2.js
@@ -50,43 +50,46 @@ const readDirectory = (pathParam) => fs.readdirSync(pathParam);
 // Funciones-------------------------------------------
 
 
-// const extractHttpLinksFromFile = (ruta) => {
-//   const concatPath = path.resolve(ruta); // obtiene la ruta absoluta del archivo
-//   const readFileMd = (pathParam) => {
-//     return new Promise((resolve, reject) => {
-//       fs.readFile(pathParam, 'utf8', (err, data) => {
-//         if (err) {
-//           reject(err);
-//         } else {
-//           const paths = []
-//           const fileToHtml = marked.parse(data);
-//           const dom = new JSDOM(fileToHtml).window.document.querySelectorAll('a');
-//           dom.forEach((el) => {
-//             if (el.href.slice(0, 3) === 'htt') {
-//               paths.push({
-//                 href: el.href,
-//                 text: (el.textContent).slice(0, 50),
-//                 file: concatPath,
-//               });
-//             }
-//           });
-//           resolve(paths);
-//         }
-//       });
-//     });
-//   };
-//   return readFileMd(concatPath).then((data) => data.flat(1));
-// }
+const extractHttpLinksFromFile = (ruta) => {
+  const concatPath = path.resolve(ruta); // obtiene la ruta absoluta del archivo
+  const readFileMd = (pathParam) => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(pathParam, 'utf8', (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          const paths = []
+          const fileToHtml = marked.parse(data);
+          const dom = new JSDOM(fileToHtml).window.document.querySelectorAll('a');
+          dom.forEach((el) => {
+            if (el.href.slice(0, 3) === 'htt') {
+              paths.push({
+                href: el.href,
+                text: (el.textContent).slice(0, 50),
+                file: concatPath,
+              });
+            }
+          });
+          resolve(paths);
+        }
+      });
+    });
+  };
+  return readFileMd(concatPath).then((data) => data.flat(1));
+}
 
 
 
-// extractHttpLinksFromFile('./leg.md')
-//   .then((paths) => {
-//     console.log(paths); // los datos reales se muestran aquí
-//   })
-//   .catch((error) => {
-//     console.error(error); // si se produce un error, se muestra aquí
-//   });
+extractHttpLinksFromFile('./leg.md')
+  .then((paths) => {
+    console.log(paths); // los datos reales se muestran aquí
+  })
+  .catch((error) => {
+    console.error(error); // si se produce un error, se muestra aquí
+  });
+
+
+// Fumcion recursiva-----------------------------------------------
 
 const findMarkdownFiles = (dirPath, results = []) => {
   const files = fs.readdirSync(dirPath);
@@ -110,58 +113,56 @@ console.log(markdownFiles);
 
 
 
+// Status del link --------------------------------------------------------------------
+
+const axios = require('axios');
+
+function linkIsActive(arrLinks) {
+  return Promise.all(arrLinks.map((link) => {
+    return axios.get(link.href)
+      .then((response) => {
+        link.message = 'Ok';
+        link.status = response.status;
+        return link;
+      })
+      .catch((error) => {
+        link.message = 'Fail';
+        link.status = error.response ? error.response.status : 'Error request';
+        return link;
+      });
+  }));
+}
+
+// const links = [
+//   { href: 'https://www.google.com', text: 'Google' },
+//   { href: 'https://www.github.com', text: 'GitHub' },
+// ];
+
+// linkIsActive(links)
+//   .then((results) => {
+//     console.log(results);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
 
 
+// toHtmlAndExtractLinks('../prueba/prueba2/prueba3/hijoDePrueba3').then((links) => {
+//   console.log("Links" *links);
+// }).catch((err) => {
+//   console.error("error" + err);
 
-// // Status del link --------------------------------------------------------------------
+// });
 
-// const axios = require('axios');
-
-// function linkIsActive(arrLinks) {
-//   return Promise.all(arrLinks.map((link) => {
-//     return axios.get(link.href)
-//       .then((response) => {
-//         link.message = 'Ok';
-//         link.status = response.status;
-//         return link;
-//       })
-//       .catch((error) => {
-//         link.message = 'Fail';
-//         link.status = error.response ? error.response.status : 'Error request';
-//         return link;
-//       });
-//   }));
-
-//   const links = [
-//     { href: 'https://www.google.com', text: 'Google' },
-//     { href: 'https://www.github.com', text: 'GitHub' },
-//   ];
-
-//   linkIsActive(links)
-//     .then((results) => {
-//       console.log(results);
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//     });
-// }
-
-// // toHtmlAndExtractLinks('../prueba/prueba2/prueba3/hijoDePrueba3').then((links) => {
-// //   console.log("Links" *links);
-// // }).catch((err) => {
-// //   console.error("error" + err);
-
-// // });
-
-// module.export = {
-//   existsPath,
-//   absolutePath,
-//   isDirectory,
-//   isFile,
-//   fileMd,
-//   readFileMd,
-//   readDirectory,
-//   toHtmlAndExtractLinks,
-//   recursiveFunction,
-//   linkIsActive,
-// };
+module.exports = {
+  existsPath,
+  absolutePath,
+  isDirectory,
+  isFile,
+  fileMd,
+  readFileMd,
+  readDirectory,
+  extractHttpLinksFromFile,
+  findMarkdownFiles,
+  linkIsActive,
+};
---
+++ prueba/mdLinks.js
@@ -0,0 +1,24 @@
+const api = require('./api.js');
+
+const mdLinks = (path, options) => {
+  new Promise((resolve, reject) => {
+    if (api.existsPath) {
+      api.absolutePath(path)
+      if ((api.findMarkdownFiles(path).length) > 0) {
+        const arrLinks = api.extractHttpLinksFromFile(path);
+        if (options.validate === true) {
+          api.linkIsActive(arrLinks)
+            .then((res) => resolve(res));
+        } else {
+          resolve(arrLinks);
+        }
+      } else {
+        // eslint-disable-next-line prefer-promise-reject-errors
+        reject('No Markdown (.md) files found');
+      }
+    } else {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject('The path do not exist. Did you mean --help?');
+    }
+  })
+}
